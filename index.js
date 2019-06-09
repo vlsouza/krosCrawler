44d786d45a7e03a4a56a049f60a9202f373ef746
viniciusB182 <viniciuslima76@outlook.com.br>
2019-06-09 13:43:26 -0300

    retrieving krosmaster images on the screen

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-let crawler = {
-	request: null,
-	cheerio: null,
-	fs: null,
-	init: function () {
-		crawler.request = require('request');
-		crawler.cheerio = require('cheerio');
-		crawler.fs = require('fs');
-		crawler.getKrosmasters();
-	},
-	getKrosmasters: function () {
-		crawler.request("http://krosfinder.com/pt/cards?v=scan&page=0", function (err, res, body) {
-			if (err)
-				console.log("Error: " + err);
 
-			var $ = crawler.cheerio.load(body);
+const request = require('request');
+const cheerio = require('cheerio');
+const express = require('express')
+const bodyParser = require("body-parser"); 
 
-			$("img[alt*='Front picture of']").each(function () {
-				const krosmaterName = $(this).parent().attr("title");
-				const krosmaterNameFormatted = krosmaterName.substring(16, krosmaterName.length);
+const app = express();
+app.set("view engine", "ejs"); 
+app.set("views", __dirname + "/views"); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-				console.log(krosmaterNameFormatted);
-			});
+app.listen(8080, () => { console.log("Server online on http://localhost:8080"); });
+
+const user = "Vinicius";	
+const krosmasters = getKrosmasters();
+
+app.get("/", (req, res) => { 
+	res.render("index", { username: user, krosmasters:  krosmasters }); 
+});
+
+function getKrosmasters() {
+	const krosmasters = []
+
+	request("http://krosfinder.com/pt/cards?v=scan&page=0", function (err, res, body) {
+		if (err)
+			console.log("Error: " + err)	
+
+		const $ = cheerio.load(body)
+
+		$("img[alt*='Front picture of']").each(function () {
+			const krosmaterName = $(this).parent().attr("title");
+			const krosmaterImgUrl = $(this).attr("src");
+			const krosmaterNameFormatted = krosmaterName.substring(16, krosmaterName.length);
+			
+			krosmasters.push({ name: krosmaterNameFormatted, imgUrl: krosmaterImgUrl });
 		});
-	}
-};
+	});
 
-crawler.init();
+	return krosmasters.sort(x => x.name);
+}
