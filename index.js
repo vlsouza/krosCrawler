3e43883c497c451c789863fbe3e0005bee37ae1a
viniciusB182 <viniciuslima76@outlook.com.br>
2019-06-09 00:49:42 -0300

    retrieving all krosmasters

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ let crawler = {
 		crawler.request = require('request');
 		crawler.cheerio = require('cheerio');
 		crawler.fs = require('fs');
-		crawler.getMovies();
+		crawler.getKrosmasters();
 	},
-	getMovies: function () {
+	getKrosmasters: function () {
 		crawler.request("http://krosfinder.com/pt/cards?v=scan&page=0", function (err, res, body) {
 			if (err)
 				console.log("Error: " + err);
 
 			var $ = crawler.cheerio.load(body);
 
-			$("img[alt='Front picture of Krosmaster']").each(function () {
+			$("img[alt*='Front picture of']").each(function () {
 				const krosmaterName = $(this).parent().attr("title");
 				const krosmaterNameFormatted = krosmaterName.substring(16, krosmaterName.length);
 
